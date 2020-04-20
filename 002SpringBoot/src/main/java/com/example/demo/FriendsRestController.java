510c9d06cc2ad04e63b65ae6069c1af98e55431a
Prafful Daga <prraful@gmail.com>
2020-04-20 16:37:08 +0530

    devtools

---
+++ 002SpringBoot/src/main/java/com/example/demo/FriendsRestController.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FriendsRestController {
+	
+	List<Friends> friends = new ArrayList<Friends>();
+	
+	public FriendsRestController() {
+	
+		Friends f1 = new Friends(1, "OBBO", "Chennai");
+		Friends f2 = new Friends(2, "MLANN", "Chennai");
+		Friends f3 = new Friends(3, "BNP", "Chennai");
+		Friends f4 = new Friends(4, "OWIOH", "Japan");
+		
+		friends.add(f1);
+		friends.add(f2);
+		friends.add(f3);
+		friends.add(f4);
+			
+	}
+	
+	
+	@RequestMapping("/")
+	public String welcome() {
+		return "Friends Community Ver 1.0!!!!";
+	}
+	
+	@RequestMapping("/all")
+	public List<Friends> getFriends() {
+		return friends;
+	}
+
+}
